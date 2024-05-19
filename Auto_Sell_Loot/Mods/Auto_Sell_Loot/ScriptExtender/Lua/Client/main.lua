e08019fc4c27328b759d7c12708b41d7ba40d71e
FallenStar <23637993+FallenStar08@users.noreply.github.com>
2024-05-20 01:35:20 +0200

    Make clear button red

--- Auto_Sell_Loot/Mods/Auto_Sell_Loot/ScriptExtender/Lua/Client/main.lua
+++ Auto_Sell_Loot/Mods/Auto_Sell_Loot/ScriptExtender/Lua/Client/main.lua
@@ -8,11 +8,12 @@ end)
 Mods.BG3MCM.IMGUIAPI:InsertModMenuTab(ModuleUUID, "Features", function(tabHeader)
     local buttonText = GetTranslatedString("h0b6204424dee44c689cf73887a557a2e6d5c") or "Clear List"
     local buttonDesc = GetTranslatedString("ha442f9c2c5bd427bafbbe98bc607fc21570e") or
-    "WARNING THIS WILL CLEAR THE LIST IN USE"
+        "WARNING THIS WILL CLEAR THE LIST IN USE"
     ---@type ExtuiButton
-    local myCustomWidget = tabHeader:AddButton(buttonText)
-    myCustomWidget:Tooltip():AddText(buttonDesc)
-    myCustomWidget.OnClick = function()
+    local ClearButton = tabHeader:AddButton(buttonText)
+    ClearButton:SetColor("Text", { 1, 0, 0, 1 })
+    ClearButton:Tooltip():AddText(buttonDesc)
+    ClearButton.OnClick = function()
         Net.Send("Fallen_Autosell_Button_ClearList", "doot")
     end
 end)
