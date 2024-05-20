7935b35e2f27d368a652ec3194ebd7c270853440
FallenStar <23637993+FallenStar08@users.noreply.github.com>
2024-05-20 21:53:31 +0200

    Added anti doofus measures

--- Auto_Sell_Loot/Mods/Auto_Sell_Loot/ScriptExtender/Lua/BootstrapServer.lua
+++ Auto_Sell_Loot/Mods/Auto_Sell_Loot/ScriptExtender/Lua/BootstrapServer.lua
@@ -1,3 +1,14 @@
 Ext.Require("Shared/_Init.lua")
 Ext.Require("ModInfos/_ModInfos.lua")
-Ext.Require("Server/main.lua")
+
+if Mods.BG3MCM then
+    Ext.Require("Server/main.lua")
+else
+    Ext.Osiris.RegisterListener("LevelGameplayStarted", 2, "after", function(levelName, isEditorMode)
+        DelayedCall(1000,
+            function()
+                Osi.OpenMessageBox(Osi.GetHostCharacter(),
+                    "FallenStar's AutoSeller : You don't have MCM installed you DOOFUS \n THE MOD WILL NOT WORK")
+            end)
+    end)
+end
